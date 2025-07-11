37dcb2046eadd18a44fb6684fc6cdbc4904ccd91
NikolaZ <nikolapikselovic@gmail.com>
2025-07-11 23:14:51 +0200

    First code push

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import requests
+from requests import HTTPError
+import json
+import sys
+import sys
+import os
+
+# Adding the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from creds import headers
+
+
+
+
+
---
+++ frontend/frontend.py
@@ -0,0 +1,19 @@
+#import streamlit as st
+#from main import content
+import time
+from datetime import datetime
+
+# def updater() :
+#     st.write(datetime.now())
+#     st.dataframe(content)
+#     time.sleep(5)
+#     st.rerun()
+
+# Make a box in witch I can extract user data and show user stats like :
+#
+# A pie with most champions used 
+# Tier and League
+# Last match played 
+
+if __name__ == "__main__" :
+    pass #updater()
